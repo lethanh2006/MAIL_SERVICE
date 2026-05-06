5501307412dc29be3a9b4a1a79cccd22d12cea70
lethanh23456 <thanhld2006.ptit@gmail.com>
2026-05-06 08:54:15 +0700

    thư viện nodemailer

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { startSendOtpConsumer } from './consumer.js';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+dotenv.config();
+startSendOtpConsumer();
+const app = express();
+const swaggerSpec = swaggerJsdoc({
+    definition: {
+        openapi: '3.0.0',
+        info: { title: 'Mail Service API', version: '1.0.0' },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+    apis: ['./src/routes/*.ts', './dist/routes/*.js'],
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/api/docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+app.listen(process.env.PORT, () => {
+    console.log(`Mail service is running on port ${process.env.PORT}`);
+});
+//# sourceMappingURL=index.js.map
